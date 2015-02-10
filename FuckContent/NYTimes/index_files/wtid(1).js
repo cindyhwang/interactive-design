459a9cc7714cf8a690a4b23286a8f128a6f34fc0
cindyhwang <cindyhwang93@gmail.com>
2015-02-09 22:26:12 -0500

    nytimes

--- FuckContent/NYTimes/index_files/wtid(1).js
+++
@@ -1,3 +0,0 @@
-if (typeof(Webtrends.dcss.dcsobj_0.dcsGetIdCallback)=="function"){
-Webtrends.dcss.dcsobj_0.dcsGetIdCallback({"gWtId":"1a03d11e-6ffb-498e-b0e9-81340a559840","gTempWtId":"","gWtAccountRollup":"1"});
-}
