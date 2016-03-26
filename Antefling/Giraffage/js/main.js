caea18608f40964c2bf02a737e8942ebecb5b53f
cindyhwang <cindyhwang93@gmail.com>
2016-03-25 22:49:49 -0400

    responsive edits

--- Antefling/Giraffage/js/main.js
+++ Antefling/Giraffage/js/main.js
@@ -1,17 +1,18 @@
 		$(function () {
 			var austDay = new Date();
-			austDay = new Date(austDay.getFullYear() + 0, 4-1, 03, 20, 0);
+			austDay = new Date(austDay.getFullYear() + -1, 4-1, 03, 20, 0);
 			$('#defaultCountdown').countdown({until: austDay});
 			$('#year').text(austDay.getFullYear());
 			});
 
 
 			$(document).ready(function(){
-			$("div").delay(500).fadeIn(500); 
-			$("img").delay(1000).fadeIn(500);
-			$("h1").delay(1500).fadeIn(500);
-			$("h2").delay(2000).fadeIn(500);
-			$("h5").delay(2500).fadeIn(500);
+
+			$("img").delay(500).fadeIn(500); 
+			$("h1").delay(1000).fadeIn(500);
+			$("h2").delay(1500).fadeIn(500);
+			$("h5").delay(2000).fadeIn(500);
+			$("div").delay(2500).fadeIn(500);
 
 			});
 
--- Antefling/Main/js/main.js
+++ Antefling/Main/js/main.js
@@ -2,12 +2,13 @@
 
 
 			$(document).ready(function(){
-			$("div").delay(500).fadeIn(500); 
-			$("img").delay(1000).fadeIn(500);
-			$("h1").delay(1500).fadeIn(500);
-			$("h2").delay(2000).fadeIn(500);
-			$("h5").delay(2500).fadeIn(500);
-			$("h6").delay(3000).fadeIn(500);
+			$("h6").delay(0).fadeIn(0);
+			$("img").delay(500).fadeIn(500);
+			$("h1").delay(1000).fadeIn(500);
+			$("h2").delay(1500).fadeIn(500);
+			$("h5").delay(2000).fadeIn(250);
+			$("div").delay(2500).fadeIn(250);
+			
 
 			});
 
