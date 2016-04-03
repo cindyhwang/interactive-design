dfd4dbee5ecd4d47f94386fd325634b80ef38fbd
cindyhwang <cindyhwang93@gmail.com>
2016-04-03 16:40:39 -0400

    edits

--- Antefling/Mitski/js/main.js
+++ Antefling/Mitski/js/main.js
@@ -7,11 +7,9 @@
 
 
 			$(document).ready(function(){
-			$("div").delay(500).fadeIn(500); 
-			$("img").delay(1000).fadeIn(500);
-			$("h1").delay(1500).fadeIn(500);
-			$("h2").delay(2000).fadeIn(500);
-			$("h5").delay(2500).fadeIn(500);
+			$("img").delay(500).fadeIn(500);
+			$("h1").delay(1000).fadeIn(500);
+			$("h2").delay(1500).fadeIn(500);
 
 			});
 
